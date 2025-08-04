18b67475a70b858d6c95604b1ed55ac3e920bf2e
saigayathri04 <155886861+saigayathri04@users.noreply.github.com>
2025-08-04 14:15:45 +0530

    Update linkedin_page.py

--- linkedin_ui_api_automation/linkedin_page.py
+++ linkedin_ui_api_automation/linkedin_page.py
@@ -9,13 +9,13 @@ class LinkedInLoginPage:
     def login(self, username, password):
         self.browser.wait_for_element(By.ID, 'username').send_keys(username)
         self.browser.wait_for_element(By.ID, 'password').send_keys(password)
-        print("✅ Username and password entered")
+        print("Username and password entered")
 
         try:
             WebDriverWait(self.browser.browser, 15).until(
                 EC.element_to_be_clickable((By.CSS_SELECTOR, 'button[type="submit"]'))
             ).click()
-            print("✅ Clicked login button")
+            print("Clicked login button")
         except Exception as e:
-            print("❌ Could not click login button:", e)
+            print("Could not click login button:", e)
             raise
